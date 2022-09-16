59cd959a269c094595615542a8e992ca2634119b
Willian Souza Nascimento <willian_2801@hotmail.com>
2022-09-16 10:50:23 -0300

    navbar

---
+++ shopify/script.js
@@ -0,0 +1,7 @@
+
+
+document.querySelector(".dark-mode").addEventListener("click", () => {
+    document.querySelector(".sun-logo").classList.toggle("animate-sun");
+    document.querySelector(".moon-logo").classList.toggle("animate-moon");
+    document.querySelector("body").classList.toggle("dark");
+})

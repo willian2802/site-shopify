9076a080bb90958d4440df7d5c60e9ffc68ff928
Willian Souza Nascimento <67116642+willian2802@users.noreply.github.com>
2022-09-25 13:18:35 -0300

    Delete shopify directory

--- shopify/script.js
+++
@@ -1,7 +0,0 @@
-
-
-document.querySelector(".dark-mode").addEventListener("click", () => {
-    document.querySelector(".sun-logo").classList.toggle("animate-sun");
-    document.querySelector(".moon-logo").classList.toggle("animate-moon");
-    document.querySelector("body").classList.toggle("dark");
-})
